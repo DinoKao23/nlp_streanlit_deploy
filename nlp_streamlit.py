dae61c5cfe656583b15f6bcf480eba24fd67de24
Tsai Lieh Kao <144483922+DinoKao23@users.noreply.github.com>
2024-04-21 16:27:57 -0700

    Update nlp_streamlit.py

--- nlp_streamlit.py
+++ nlp_streamlit.py
@@ -63,7 +63,8 @@ def pipeline(comments, model_path):
     return prediction_df
 
 def main():
-    st.title("Movie Comment Sentiment Analysis")
+    st.header("Movie Comment Sentiment Analysis🍅")
+    st.title("Know Your Audience From This Place.")
     st.write("With the help of BERT LLM, you can predict the sentiment of a movie reviews. By collecting comments from Rotten Tomatoes Audiences, we can see how people react to the movies and predict the sentiment of the comments.")
 
     user_input = st.text_input("Test the comment:","You could HEAR the disappointment from the audience I was with when that â€œTo Be Continuedâ€ title card showed up lmao. But besides that, yeah this was a dang fine sequel. Better than most I dare say. Seeing aged-up Peni Parker in her upgraded mech for the first time almost made me tear up a bit.Oh yeah and Hailee Steinfeld deserves an Oscar for her performance here, all of the scenes when she's paired up with her dad were beautifully executed. Easily the best parts of the film for me personally.")
